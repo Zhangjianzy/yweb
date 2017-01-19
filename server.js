0f0b77e85ee5ac602286c4e07ae1b0ade0c77723
张舰 <zhangjianzy@qq.com>
2017-01-19 21:40:52 +0800

    适应openshift

--- server.js
+++ server.js
@@ -1,11 +1,2 @@
-import express from 'express';
-import ServerRoutes from './server/routes';
-
-let app = express();
-app.set('port', 3100);
-
-ServerRoutes(app);
-
-app.listen(app.get('port'), ()=> {
-  console.log('yweb server listening on port ' + app.get('port'));
-});
+require('babel-register');
+require('./server');
--- start.js
+++ start.js
@@ -1,2 +1,12 @@
-require('babel-register');
-require('./server');
+import express from 'express';
+import fs from 'fs';
+import ServerRoutes from './server/routes';
+
+let app = express();
+app.set('port', 3100);
+
+ServerRoutes(app);
+
+app.listen(app.get('port'), ()=> {
+  console.log('yweb server listening on port ' + app.get('port'));
+});
